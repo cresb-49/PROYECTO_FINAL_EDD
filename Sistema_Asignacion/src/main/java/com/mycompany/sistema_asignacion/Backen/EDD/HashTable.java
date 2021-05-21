60813569ab323e3eca5e3f7241e1933a1c0c13c5
Benjamin <benjamin@pop-os.localdomain>
2021-05-20 23:49:10 -0600

    Solucion temporal de error de tabla hash

--- Sistema_Asignacion/src/main/java/com/mycompany/sistema_asignacion/Backen/EDD/HashTable.java
+++ Sistema_Asignacion/src/main/java/com/mycompany/sistema_asignacion/Backen/EDD/HashTable.java
@@ -4,6 +4,7 @@ import com.mycompany.sistema_asignacion.Backen.Exceptions.CloneNodeException;
 import com.mycompany.sistema_asignacion.Backen.Exceptions.NotFoundNodeException;
 
 public class HashTable<T> {
+
     private static final double MAX_CAPACITY = 0.55;
     private int items;
     private int size;
@@ -19,7 +20,7 @@ public class HashTable<T> {
 
     /**
      * Agrega un elemento a la tabla hash
-     * 
+     *
      * @param data
      * @param hash
      * @throws CloneNodeException
@@ -28,21 +29,23 @@ public class HashTable<T> {
         int index = hash % this.size;
         if (this.array[index] == null) {
             NodeHash<T> nuevo = new NodeHash<>(data);
-            System.out.println("Elemento agregado en posicion: " + index);
+            System.out.println("Elemento con hash " + hash + " agregado en posicion: " + index);
             this.array[index] = nuevo;
             this.items++;
             this.factorDeCarga = ((double) this.items / this.size);
             System.out.println("Factor de carga: " + this.factorDeCarga);
             if (this.factorDeCarga > MAX_CAPACITY) {
                 System.out.println("Se necesita rehash");
+                this.reHash();
+                System.out.println("Rehash Ejecutado");
             }
         } else {
             NodeHash<T> tmp = (NodeHash<T>) array[index];
             boolean igual = (tmp.getData().equals(data));
-            System.out.println("Iguales: "+igual);
+            System.out.println("Iguales: " + igual);
             if (!igual) {
-                System.out.println("Colicion de elemento posicion: " + index);
-                this.solucionarColicion(data, hash, 1,true);
+                System.out.println("Colicion de elemento con hash " + hash + " posicion: " + index);
+                this.solucionarColicion(data, hash, 1, true);
                 tmp.setReHashBridge(true);
             } else {
                 throw new CloneNodeException("Ya existe un elemento igual en la tabla");
@@ -52,43 +55,52 @@ public class HashTable<T> {
 
     /**
      * Soluciona el momento de la colision con el array
-     * 
+     *
      * @param data
      * @param hash
      * @param i
      * @throws CloneNodeException
      */
-    private void solucionarColicion(T data, int hash, int i,boolean option) throws CloneNodeException {
+    private void solucionarColicion(T data, int hash, int i, boolean option) throws CloneNodeException {
         i = i + 1;
+        System.out.println("Hash: " + hash + " size: " + this.size);
         int index = ((hash % this.size) * i);
+
+        System.out.println("Generado: " + index);
 
         while (index >= this.size) {
             index = index - this.size;
         }
+        System.out.println("Posicion Nueva: " + index);
 
         if (this.array[index] == null) {
             NodeHash<T> nuevo = new NodeHash<>(data);
-            System.out.println("Elemento agregado en posicion: " + index);
+            System.out.println("Elemento con hash " + hash + " agregado en posicion: " + index);
             this.array[index] = nuevo;
             this.items++;
             this.factorDeCarga = ((double) this.items / this.size);
             System.out.println("Factor de carga: " + this.factorDeCarga);
-            if(option){
+            if (option) {
                 if (this.factorDeCarga > MAX_CAPACITY) {
                     System.out.println("Se necesita rehash");
                     this.reHash();
+                    System.out.println("Rehash ejecutado");
                 }
             }
         } else {
-            NodeHash<T> tmp = (NodeHash<T>) array[index];
-            boolean igual = (tmp.getData().equals(data));
-            System.out.println("Iguales: "+igual);
-            if (!igual) {
-                System.out.println("Colicion de elemento posicion: " + index);
-                this.solucionarColicion(data, hash, i,option);
-                tmp.setReHashBridge(true);
-            } else {
-                throw new CloneNodeException("Ya existe un elemento igual en la tabla");
+            if (index != 0) {
+                NodeHash<T> tmp = (NodeHash<T>) array[index];
+                boolean igual = (tmp.getData().equals(data));
+                System.out.println("Iguales: " + igual);
+                if (!igual) {
+                    System.out.println("Colicion de elemento con hash " + hash + " posicion: " + index);
+                    this.solucionarColicion(data, hash, i, option);
+                    tmp.setReHashBridge(true);
+                } else {
+                    throw new CloneNodeException("Ya existe un elemento igual en la tabla");
+                }
+            }else{
+                System.out.println("Elemento con index 0 el algoritmo no resuleve coliciones index 0 hash del error: "+hash);
             }
         }
     }
@@ -100,34 +112,36 @@ public class HashTable<T> {
         //Copia de los elementos del hash anterior
         Object copiaArray[] = this.array;
         //Nuevo tamaño para la tabla hash 
-        this.size = this.size + 21;
+        this.size = this.obenerPrimo(this.size);
+        System.out.println("Nuevo Size: " + this.size);
         this.array = new Object[this.size];
-        this.items =0;
+        this.items = 0;
 
         for (Object copi : copiaArray) {
             NodeHash<T> antiguo = (NodeHash<T>) copi;
-            if (!antiguo.isDelete()) {
-                //PARAMETROS PARA EL NUEVO HASH
-                int hash = antiguo.getData().hashCode();
-                T data = antiguo.getData();
-                int index = hash % this.size;
-                //FIN PARAMETROS PARA EL NUEVO HASH
-
-                if (this.array[index] == null) {
-                    NodeHash<T> nuevo = new NodeHash<>(data);
-                    System.out.println("Elemento agregado en posicion: " + index);
-                    this.array[index] = nuevo;
-                    this.items++;
-                    this.factorDeCarga = ((double) this.items / this.size);
-                    System.out.println("Factor de carga: " + this.factorDeCarga);
-                } else {
-                    NodeHash<T> tmp = (NodeHash<T>) array[index];
-                    if (!tmp.getData().equals(data)) {
-                        System.out.println("Colicion de elemento posicion: " + index);
-                        this.solucionarColicion(data, hash, 1,false);
-                        tmp.setReHashBridge(true);
+            if (antiguo != null) {
+                if (!antiguo.isDelete()) {
+                    //PARAMETROS PARA EL NUEVO HASH
+                    int hash = antiguo.getData().hashCode();
+                    T data = antiguo.getData();
+                    int index = hash % this.size;
+                    //FIN PARAMETROS PARA EL NUEVO HASH
+                    if (this.array[index] =Rehash ejecutado= null) {
+                        NodeHash<T> nuevo = new NodeHash<>(data);
+                        System.out.println("Elemento con hash " + hash + " agregado en posicion: " + index);
+                        this.array[index] = nuevo;
+                        this.items++;
+                        this.factorDeCarga = ((double) this.items / this.size);
+                        System.out.println("Factor de carga: " + this.factorDeCarga);
                     } else {
-                        throw new CloneNodeException("Ya existe un elemento igual en la tabla");
+                        NodeHash<T> tmp = (NodeHash<T>) array[index];
+                        if (!tmp.getData().equals(data)) {
+                            System.out.println("Colicion de elemento posicion: " + index);
+                            this.solucionarColicion(data, hash, 1, false);
+                            tmp.setReHashBridge(true);
+                        } else {
+                            throw new CloneNodeException("Ya existe un elemento igual en la tabla");
+                        }
                     }
                 }
             }
@@ -136,29 +150,30 @@ public class HashTable<T> {
 
     /**
      * Busaca un elemento en la tabla hash
+     *
      * @param busqueda
      * @param hash
      * @return Objeto / null
      */
-    public T buscar(T busqueda,int hash){
+    public T buscar(T busqueda, int hash) {
         int index = hash % this.size;
 
         NodeHash<T> tmp = (NodeHash<T>) this.array[index];
 
-        if(tmp==null){
+        if (tmp == null) {
             return null;
-        }else{
+        } else {
             T result = tmp.getData();
-            if(result.equals(busqueda)){
-                if(!tmp.isDelete()){
+            if (result.equals(busqueda)) {
+                if (!tmp.isDelete()) {
                     return result;
-                }else{
+                } else {
                     return null;
                 }
-            }else{
-                if(tmp.isReHashBridge()){
+            } else {
+                if (tmp.isReHashBridge()) {
                     return this.busquedaReHash(busqueda, hash, 1);
-                }else{
+                } else {
                     return null;
                 }
             }
@@ -167,12 +182,13 @@ public class HashTable<T> {
 
     /**
      * Metodo recursivo para la busqueda de un elemento en la tabla hash
+     *
      * @param busqueda
      * @param hash
      * @param i
      * @return
      */
-    private T busquedaReHash(T busqueda, int hash, int i){
+    private T busquedaReHash(T busqueda, int hash, int i) {
 
         i = i + 1;
         int index = ((hash % this.size) * i);
@@ -181,20 +197,20 @@ public class HashTable<T> {
         }
 
         NodeHash<T> tmp = (NodeHash<T>) this.array[index];
-        if(tmp==null){
+        if (tmp == null) {
             return null;
-        }else{
+        } else {
             T result = tmp.getData();
-            if(result.equals(busqueda)){
-                if(!tmp.isDelete()){
+            if (result.equals(busqueda)) {
+                if (!tmp.isDelete()) {
                     return result;
-                }else{
+                } else {
                     return null;
                 }
-            }else{
-                if(tmp.isReHashBridge()){
+            } else {
+                if (tmp.isReHashBridge()) {
                     return this.busquedaReHash(busqueda, hash, i);
-                }else{
+                } else {
                     return null;
                 }
             }
@@ -202,28 +218,29 @@ public class HashTable<T> {
     }
 
     /**
-     * Elimina un dato de la tabla hash en base a un modelo 
+     * Elimina un dato de la tabla hash en base a un modelo
+     *
      * @param dato
      * @param hash
      * @throws NotFoundNodeException
      */
-    public void eliminar(T dato,int hash) throws NotFoundNodeException{
+    public void eliminar(T dato, int hash) throws NotFoundNodeException {
         int index = hash % this.size;
         NodeHash<T> tmp = (NodeHash<T>) this.array[index];
-        if(tmp==null){
+        if (tmp == null) {
             throw new NotFoundNodeException("No existe el elemento buscado");
-        }else{
+        } else {
             T result = tmp.getData();
-            if(result.equals(dato)){
-                if(tmp.isDelete()){
+            if (result.equals(dato)) {
+                if (tmp.isDelete()) {
                     throw new NotFoundNodeException("No existe el elemento buscado");
-                }else{
+                } else {
                     tmp.setDelete(true);
                 }
-            }else{
-                if(tmp.isReHashBridge()){
+            } else {
+                if (tmp.isReHashBridge()) {
                     this.eliminarReHash(dato, hash, 1);
-                }else{
+                } else {
                     throw new NotFoundNodeException("No existe el elemento buscado");
                 }
             }
@@ -232,45 +249,46 @@ public class HashTable<T> {
 
     /**
      * Metodo recursivo para eliminar un dato de la tabla hash
+     *
      * @param dato
      * @param hash
      * @param i
      * @throws NotFoundNodeException
      */
-    public void eliminarReHash(T dato, int hash, int i) throws NotFoundNodeException{
+    public void eliminarReHash(T dato, int hash, int i) throws NotFoundNodeException {
         i = i + 1;
         int index = ((hash % this.size) * i);
         while (index >= this.size) {
             index = index - this.size;
         }
         NodeHash<T> tmp = (NodeHash<T>) this.array[index];
-        if(tmp==null){
+        if (tmp == null) {
             throw new NotFoundNodeException("No existe el elemento buscado");
-        }else{
+        } else {
             T result = tmp.getData();
-            if(result.equals(dato)){
-                if(tmp.isDelete()){
+            if (result.equals(dato)) {
+                if (tmp.isDelete()) {
                     throw new NotFoundNodeException("No existe el elemento buscado");
-                }else{
+                } else {
                     tmp.setDelete(true);
                 }
-            }else{
-                if(tmp.isReHashBridge()){
+            } else {
+                if (tmp.isReHashBridge()) {
                     this.eliminarReHash(dato, hash, i);
-                }else{
+                } else {
                     throw new NotFoundNodeException("No existe el elemento buscado");
                 }
             }
         }
     }
 
-    public String generarDotCode(){
-        String code ="nodesep=.05;\nrankdir=LR;\nnode [shape=record,width=.1,height=.1];\n";
+    public String generarDotCode() {
+        String code = "nodesep=.05;\nrankdir=LR;\nnode [shape=record,width=.1,height=.1];\n";
 
         code = code + "nodeHASH [label = \"";
         for (int i = 0; i < array.length; i++) {
-            code = code + "<f"+i+">"+i;
-            if(i!=(array.length-1)){
+            code = code + "<f" + i + ">" + i;
+            if (i != (array.length - 1)) {
                 code = code + "|";
             }
         }
@@ -280,10 +298,10 @@ public class HashTable<T> {
         Object tmp;
         for (int i = 0; i < array.length; i++) {
             tmp = array[i];
-            if(tmp!=null){
-                if(tmp instanceof NodeHash){
-                    if(!((NodeHash<T>)tmp).isDelete()){
-                        code = code +"node"+i+" [label = \"{ <f0>"+tmp.toString()+"}\"];\n";
+            if (tmp != null) {
+                if (tmp instanceof NodeHash) {
+                    if (!((NodeHash<T>) tmp).isDelete()) {
+                        code = code + "node" + i + " [label = \"{ <f0>" + tmp.toString() + "}\"];\n";
                     }
                 }
             }
@@ -291,10 +309,10 @@ public class HashTable<T> {
 
         for (int i = 0; i < array.length; i++) {
             tmp = array[i];
-            if(tmp!=null){
-                if(tmp instanceof NodeHash){
-                    if(!((NodeHash<T>)tmp).isDelete()){
-                        code = code +"nodeHASH:f"+i+" -> node"+i+":f0;\n";
+            if (tmp != null) {
+                if (tmp instanceof NodeHash) {
+                    if (!((NodeHash<T>) tmp).isDelete()) {
+                        code = code + "nodeHASH:f" + i + " -> node" + i + ":f0;\n";
                     }
                 }
             }
@@ -302,20 +320,40 @@ public class HashTable<T> {
         return code;
     }
 
-
     /**
      * Imprime la tabla hash
      */
     public void imprimirHash() {
 
-        NodeHash<T> tmp =null;
+        NodeHash<T> tmp = null;
         for (int i = 0; i < array.length; i++) {
             tmp = (NodeHash<T>) array[i];
-            if(tmp !=null){
-                if(!tmp.isDelete()){
+            if (tmp != null) {
+                if (!tmp.isDelete()) {
                     System.out.println("key: " + i + " -> \n" + array[i]);
                 }
             }
+        }
+    }
+
+    /**
+     * Obtiene el numero primo siguiente
+     *
+     * @param actual
+     * @return
+     */
+    int obenerPrimo(int actual) {
+        int result = actual + 1;
+        int contador = 0;
+        for (int i = result; i > 0; i--) {
+            if (result % i == 0) {
+                contador++;
+            }
+        }
+        if (contador == 2) {
+            return result;
+        } else {
+            return this.obenerPrimo(result);
         }
     }
 
@@ -378,5 +416,4 @@ public class HashTable<T> {
             return this.data.toString();
         }
     }
-
 }
--- Sistema_Asignacion/src/main/java/com/mycompany/sistema_asignacion/main.java
+++ Sistema_Asignacion/src/main/java/com/mycompany/sistema_asignacion/main.java
@@ -1,18 +1,72 @@
 package com.mycompany.sistema_asignacion;
 
+import com.mycompany.sistema_asignacion.Backen.EDD.HashTable;
 import com.mycompany.sistema_asignacion.Backen.Exceptions.CloneNodeException;
 import com.mycompany.sistema_asignacion.Backen.Objetos.DatosSistema;
+import com.mycompany.sistema_asignacion.Backen.Objetos.Estudiante;
 import com.mycompany.sistema_asignacion.Backen.Objetos.Usuario;
 import com.mycompany.sistema_asignacion.Fronted.UI.FramePrincipal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.text.TabableView;
 
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args){/*
         DatosSistema datos = new DatosSistema(); 
+        
         try {
             datos.getUsuarios().add(new Usuario(0, "AdminSistema","12345","super"), "AdminSistema");
         } catch (CloneNodeException e) {
             e.printStackTrace();
         }
         FramePrincipal frame = new FramePrincipal(datos); frame.setVisible(true);
+        */
+        prubas();
+    }
+    
+    private static void prubas(){
+        
+        HashTable<Estudiante> tabla = new HashTable<>();
+        
+        try {
+            tabla.add(new Estudiante(682129680, "", ""), 682129680);
+            tabla.add(new Estudiante(485667018, "", ""), 485667018);
+            tabla.add(new Estudiante(510648777, "", ""), 510648777);
+            tabla.add(new Estudiante(134403629, "", ""), 134403629);
+            tabla.add(new Estudiante(576587791, "", ""), 576587791);
+            tabla.add(new Estudiante(514029522, "", ""), 514029522);
+            tabla.add(new Estudiante(971596049, "", ""), 971596049);
+            tabla.add(new Estudiante(652719362, "", ""), 652719362);
+            tabla.add(new Estudiante(292756392, "", ""), 292756392);
+            tabla.add(new Estudiante(591120081, "", ""), 591120081);
+            tabla.add(new Estudiante(487587175, "", ""), 487587175);
+            tabla.add(new Estudiante(643606791, "", ""), 643606791);
+            tabla.add(new Estudiante(148897761, "", ""), 148897761);
+            tabla.add(new Estudiante(286419336, "", ""), 286419336);
+            tabla.add(new Estudiante(749974348, "", ""), 749974348);
+            tabla.add(new Estudiante(513077560, "", ""), 513077560);
+            tabla.add(new Estudiante(340663950, "", ""), 340663950);
+            tabla.add(new Estudiante(509208002, "", ""), 509208002);
+            tabla.add(new Estudiante(850916192, "", ""), 850916192);
+            tabla.add(new Estudiante(347699075, "", ""), 347699075);
+            tabla.add(new Estudiante(380898177, "", ""), 380898177);
+            tabla.add(new Estudiante(149034226, "", ""), 149034226);
+            tabla.add(new Estudiante(803294069, "", ""), 803294069);
+            tabla.add(new Estudiante(188570239, "", ""), 188570239);
+            tabla.add(new Estudiante(225882611, "", ""), 225882611);
+            tabla.add(new Estudiante(564758863, "", ""), 564758863);
+            tabla.add(new Estudiante(771328212, "", ""), 771328212);
+            tabla.add(new Estudiante(283234134, "", ""), 283234134);
+            tabla.add(new Estudiante(375150001, "", ""), 375150001);
+            tabla.add(new Estudiante(168031093, "", ""), 168031093);
+            tabla.add(new Estudiante(301, "", ""), 301);
+            tabla.add(new Estudiante(302, "", ""), 302);
+            tabla.add(new Estudiante(303, "", ""), 303);
+            tabla.imprimirHash();
+        } catch (CloneNodeException ex) {
+            ex.printStackTrace();
+        }
+        
+        
     }
 }
